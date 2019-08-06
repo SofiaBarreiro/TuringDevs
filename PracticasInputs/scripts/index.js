c20d5b41d305b098a9545e30d6cc615607211c14
sofiadb <barreirosofia@hotmail.com>
2019-08-05 21:23:41 -0300

    terminando practicaInputs

--- PracticasInputs/scripts/index.js
+++ PracticasInputs/scripts/index.js
@@ -1,29 +1,48 @@
 window.addEventListener('load', function () {
 
-
+    var countColors =0;
+    var color1 = 'black';
+    var color2 = 'black';
+    var color3 = 'black';
+    var color4 = 'black';
     var SelectedColor = document.getElementById('colorSelect');
 
 
-    var body = document.getElementById('body');
-    // console.log(SelectedColor.value);
-    console.log(body);
+    SelectedColor.addEventListener('change', function(load){
+        countColors ++;
+       switch(countColors){
+
+        case 1 :
+        color1= SelectedColor.value;
+        var button1 = document.getElementById('button1');
+        button1.style.background = color1;
+        break;
+        case 2:
+        color2 = SelectedColor.value;
+        var button1 = document.getElementById('button2');
+        button1.style.background= color2;
+        break;
+        case 3:
+        color3 = SelectedColor.value;
+        var button1 = document.getElementById('button3');
+        button1.style.background= color3;
+        break;
+        case 4:
+        color4 = SelectedColor.value;
+        var button1 = document.getElementById('button4');
+        button1.style.background= color4;
 
 
-    SelectedColor.addEventListener('change', function(){
+        break;
+        default:
+        break;
+       }
 
-        document.body.style.background = SelectedColor.value;
+        document.body.style.background = 'linear-gradient('+ 'to right' + ', ' + color1 + ', '+  color2 + ', ' +color3 + ', '+color4 + ')';
         load.preventDefault();
 
-
-    })
+    });
        
-   var inputUpLoad = document.getElementById('fileUpload');
-   inputUpLoad.addEventListener('change', function(){
-
-    handleFiles(this.files);
-    load.preventDefault();
-   });
-
 
 
  
